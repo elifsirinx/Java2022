76895e7707f7c5bfbeb739b7446e2a74473a7851
elifsirin42 <elifsirin42@gmail.com>
2022-09-16 21:09:35 +0300

    Java Dersi 13 : Do-While Döngüsüyle Çalışmak

---
+++ JavaCourse/src/intro/Ders13.java
@@ -0,0 +1,17 @@
+package intro;
+
+public class Ders13 {
+
+	public static void main(String[] args) {
+		// Do-While
+		int j=100;
+		do {
+			System.out.println("Loglandı");
+			System.out.println(j);
+			j+=2;
+		}while(j<10);
+		System.out.println("Do-While Döngüsü Bitti");
+		}
+
+	
+}
