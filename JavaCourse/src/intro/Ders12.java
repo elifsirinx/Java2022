abbfa3e3d37c294ac7c49aff877c9e2ce0fb3d97
elifsirin42 <elifsirin42@gmail.com>
2022-09-16 21:05:51 +0300

    Java Dersi 12 : While Döngüsüyle Çalışmak

---
+++ JavaCourse/src/intro/Ders12.java
@@ -0,0 +1,17 @@
+package intro;
+
+public class Ders12 {
+
+	public static void main(String[] args) {
+		int i=1;
+		//While
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		
+		System.out.println("While Döngüsü Bitti");
+
+	}
+
+}
