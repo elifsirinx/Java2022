66293882e772900642d10ad75bebc75d15bc5180
elifsirin42 <elifsirin42@gmail.com>
2022-09-16 20:25:22 +0300

    Java Dersi 6 : Değişkenler ve Değişken Kullanım İhtiyacı

---
+++ JavaCourse/src/intro/Ders6.java
@@ -0,0 +1,16 @@
+package intro;
+
+public class Ders6 {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayısı : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayısı : " + ogrenciSayisi);
+		System.out.println("Öğrenci sayısı : " + ogrenciSayisi);
+
+	}
+
+}
