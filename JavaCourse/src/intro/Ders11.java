191c0b888ad147d52ef4688f6002f8d26b446cb1
elifsirin42 <elifsirin42@gmail.com>
2022-09-16 21:00:48 +0300

    Java Dersi 11 : For Döngüsüyle Çalışmak

---
+++ JavaCourse/src/intro/Ders11.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class Ders11 {
+
+	public static void main(String[] args) {
+		//for
+		for(int i = 2 ; i<10 ; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti");
+
+	}
+
+}
