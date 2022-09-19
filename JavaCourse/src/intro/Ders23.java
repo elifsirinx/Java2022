88445a5437e5e8deb548091eadf19973e24f8971
elifsirin42 <elifsirin42@gmail.com>
2022-09-19 22:55:09 +0300

    Java Dersi 23 : Mini Proje 5 - Sayı Bulma

---
+++ JavaCourse/src/intro/Ders23.java
@@ -0,0 +1,23 @@
+package intro;
+
+public class Ders23 {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] {1,2,5,7,9,10};
+		int aranacak = 5;
+		boolean varMi = false;
+		
+		for (int sayi : sayilar) {
+			if(sayi==aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if(varMi) {
+			System.out.println("Sayı mevcuttur.");
+		}else {
+			System.out.println("Sayı mevcut değildir.");
+		}
+	}
+
+}
